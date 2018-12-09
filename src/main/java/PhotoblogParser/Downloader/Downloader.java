2aa1d94d364ae0e7f8909bb5ee44d30b085e7d71
bogusz2 <boguszewicz@mail.ru>
2018-12-09 16:00:42 +0100

    Wydzielenie klasy Downloader z metodami get.

---
+++ src/main/java/PhotoblogParser/Downloader/Downloader.java
@@ -0,0 +1,52 @@
+package PhotoblogParser.Downloader;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+import static PhotoblogParser.Config.BROWSER;
+import static PhotoblogParser.Config.NO_NAME_ENTRY;
+
+public class Downloader {
+  public Downloader() {
+    System.setProperty("http.agent", BROWSER);
+  }
+
+  protected String getHTMLPhotoNote(Document html){
+    return html.getElementById("photo_note").html();
+  }
+
+  protected String getTitle(Document html, String eNumber) {
+    String title = html.getElementsByTag("title").get(0).text();
+    if (title.equals(NO_NAME_ENTRY)) {
+      title = "Wpis " + eNumber;
+    }
+    return title;
+  }
+  protected String getDateOfEntry(Document html) {
+    return html.getElementsByClass("now_date").get(0).text();
+  }
+
+  protected byte[] getImgOfEntry(Document html) throws IOException {
+    String URL = html.getElementById("show_pic_viewer").absUrl("src");
+    java.net.URL url = new URL(URL);
+    BufferedImage originalImage = ImageIO.read(url);
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    ImageIO.write(originalImage, "jpg", baos);
+    byte[] img = baos.toByteArray();
+    baos.close();
+    return img;
+  }
+
+  protected String getNextPage(Document html) {
+    String lastEntryURL;
+    Element previous = html.getElementById("photo_nav");
+    lastEntryURL = previous.getElementsByAttribute("href").last().absUrl("href");
+    return lastEntryURL;
+  }
+}
--- src/main/java/PhotoblogParser/Downloader/DownloaderToMemory.java
+++ src/main/java/PhotoblogParser/Downloader/DownloaderToMemory.java
@@ -3,23 +3,16 @@ package PhotoblogParser.Downloader;
 import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.*;
-import java.net.URL;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
-
-
-import static PhotoblogParser.Config.*;
+import static PhotoblogParser.Config.LOCAL_PATH;
 
 
-public class DownloaderToMemory implements DownloaderEntry {
-
-  public DownloaderToMemory() {
-    System.setProperty("http.agent", BROWSER);
-  }
+public class DownloaderToMemory extends Downloader implements DownloaderEntry {
 
   @Override
   public String saveEntry(String URL, String eNumber) throws IOException {
@@ -27,19 +20,13 @@ public class DownloaderToMemory implements DownloaderEntry {
     Document html = Jsoup.connect(URL).timeout(10000).get();
     String title = getTitle(html, eNumber);
     saveTitle(title, LOCAL_PATH + eNumber);//todo same problem
-    String date = getDate(html);
+    String date = getDateOfEntry(html);
     saveDate(date, LOCAL_PATH + eNumber);
-    byte[] image = getImg(html);
+    byte[] image = getImgOfEntry(html);
     saveImage(image, LOCAL_PATH + eNumber);
-    String HTMLPhotoNote = getHTMLPhotoNote(html);
+    String HTMLPhotoNote = this.getHTMLPhotoNote(html);
     savePhotoNoteAsHTML(HTMLPhotoNote, LOCAL_PATH + eNumber);
-//    savePhotoNoteAsHTML(html, LOCAL_PATH + eNumber);
-
     return getNextPage(html);
-
-  }
-  private String getHTMLPhotoNote(Document html){
-    return html.getElementById("photo_note").html();
 
   }
 
@@ -47,37 +34,11 @@ public class DownloaderToMemory implements DownloaderEntry {
     FileUtils.writeByteArrayToFile(new File(path + ".jpg"), image);
   }
 
-
-  private String getTitle(Document html, String eNumber) {
-    String title = html.getElementsByTag("title").get(0).text();
-    if (title.equals(NO_NAME_ENTRY)) {
-      title = "Wpis " + eNumber;
-    }
-    return title;
-  }
-
-  private String getDate(Document html) {
-    return html.getElementsByTag("title").get(0).text();
-  }
-
-
   private void saveTitle(String title, String path) throws IOException {// todo: save date separetly
     File dateTitlePost = new File(path + ".txt");
     BufferedWriter bwDateTitlePost = new BufferedWriter(new FileWriter(dateTitlePost));
     bwDateTitlePost.write(title);
     bwDateTitlePost.close();
-  }
-
-
-  private byte[] getImg(Document html) throws IOException {
-    String URL = html.getElementById("show_pic_viewer").absUrl("src");
-    URL url = new URL(URL);
-    BufferedImage originalImage = ImageIO.read(url);
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    ImageIO.write(originalImage, "jpg", baos);
-    byte[] img = baos.toByteArray();
-    baos.close();
-    return img;
   }
 
   private void saveDate(String date, String path) throws IOException {
@@ -88,31 +49,10 @@ public class DownloaderToMemory implements DownloaderEntry {
     bwDateTitlePost.close();
   }
 
-
-//  private void saveImageAs(Document html, String name) throws IOException {
-//    String imgURL = html.getElementById("show_pic_viewer").absUrl("src");
-//    URL imURL = new URL(imgURL);
-//    InputStream in = imURL.openStream();
-//    OutputStream out = new BufferedOutputStream(new FileOutputStream(LOCAL_PATH + name + ".jpg"));
-//    for (int b; (b = in.read()) != -1; ) {
-//      out.write(b);
-//    }
-//    out.close();
-//    in.close();
-//  }
-
   private void savePhotoNoteAsHTML(String HTMLPhotoNote, String path) throws IOException {
     File post = new File(path + ".html");
     BufferedWriter bw = new BufferedWriter(new FileWriter(post));
     bw.write(HTMLPhotoNote);
     bw.close();
   }
-
-  private String getNextPage(Document html) {
-    String lastEntryURL;
-    Element previous = html.getElementById("photo_nav");
-    lastEntryURL = previous.getElementsByAttribute("href").last().absUrl("href");
-    return lastEntryURL;
-  }
-
 }
